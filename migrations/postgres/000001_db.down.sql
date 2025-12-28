3e8b856b64fcf3cac09269356bef891175e4451b
bondarenkozahar80-hub <bondarenkozahar80@gmail.com>
2025-12-28 13:49:54 +0300

    Create 000001_db.down.sql

---
+++ migrations/postgres/000001_db.down.sql
@@ -0,0 +1,10 @@
+DROP INDEX IF EXISTS idx_clicks_short_created;
+DROP INDEX IF EXISTS idx_clicks_browser;
+DROP INDEX IF EXISTS idx_clicks_os;
+DROP INDEX IF EXISTS idx_clicks_device;
+
+
+DROP TABLE IF EXISTS clicks;
+
+
+DROP TABLE IF EXISTS urls;
